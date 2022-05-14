f3cd7feb861efd53cf703e4301210d5de740e545
mmmalhammami <mmmalhammami@sm.imamu.edu.sa>
2022-05-14 04:00:51 +0300

    Slight changes

--- Driver_Distraction_Detection.py
+++ Driver_Distraction_Detection.py
@@ -6,8 +6,13 @@ import RPi.GPIO as GPIO2
 import tkinter as tk
 import numpy as np
 import threading
+import warnings
 import cv2
+import os
 
+os.environ['KERAS_BACKEND'] = 'tensorflow'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # 3 = INFO, WARNING, and ERROR
+warnings.filterwarnings('ignore')
 
 ########################################################################################################################
 # List of Module functions: -
